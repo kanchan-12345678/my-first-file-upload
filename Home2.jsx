05fb798afdc8dbb746f6fe11b09780a38f911c2a
kanchan-12345678 <kkumari89635@gmail.com>
2026-02-20 12:58:53 +0530

    add new file

---
+++ Home2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home2 = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui debitis minima quo explicabo excepturi 
+      tempora aspernatur consequuntur ab beatae, vel laborum reiciendis dolorem accusamus voluptates perspiciatis consectetur esse dolor nihil.
+    </div>
+  )
+}
+
+export default Home2
